10f0974b14806acc314b2a2c90aa032ebdc850d8
Jason Jones <jsjones96@gmail.com>
2024-08-29 20:05:36 -0700

    feat: add simple logger; sort pom files

--- tutorialapi-server/src/main/java/com/jasonsjones/server/ApiServer.java
+++ tutorialapi-server/src/main/java/com/jasonsjones/server/ApiServer.java
@@ -1,12 +1,16 @@
 package com.jasonsjones.server;
 
 import org.eclipse.jetty.server.HttpConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.eclipse.jetty.http.HttpScheme.HTTPS;
 
 public class ApiServer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ApiServer.class);
+
     public static void main(String... args) {
-        System.out.println("Hello App Server!");
+        LOGGER.info("Starting app server...");
 
         HttpConfiguration httpsConfiguration = new HttpConfiguration();
         httpsConfiguration.setSecureScheme(HTTPS.asString());
