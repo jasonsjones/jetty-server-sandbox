7c0a0e2e32ba0c7f33ae54b4958295171a652214
Jason Jones <jsjones96@gmail.com>
2024-09-04 19:49:57 -0700

    refactor: move strings to static final private vars

--- tutorialapi-server/src/main/java/com/jasonsjones/server/ApiServer.java
+++ tutorialapi-server/src/main/java/com/jasonsjones/server/ApiServer.java
@@ -16,6 +16,10 @@ import static org.eclipse.jetty.http.HttpVersion.HTTP_1_1;
 
 public class ApiServer {
     private static final Logger LOGGER = LoggerFactory.getLogger(ApiServer.class);
+    private static final String KEYSTORE_PATH = "tutorialapi-server/src/main/resources/certs/site.p12";
+    private static final String KEYSTORE_TYPE = "PKCS12";
+    private static final String KEYSTORE_PASSWORD = "changeit";
+    private static final String WEB_ROOT_PATH = "tutorialapi-server/src/main/resources/web";
 
     public static void main(String... args) throws Exception {
 
@@ -28,10 +32,10 @@ public class ApiServer {
 
         LOGGER.info("Setting up SSL (server) context factory");
         SslContextFactory.Server sslContextFactory = new SslContextFactory.Server();
-        sslContextFactory.setKeyStorePath("tutorialapi-server/src/main/resources/certs/site.p12");
-        sslContextFactory.setKeyStoreType("PKCS12");
-        sslContextFactory.setKeyStorePassword("changeit");
-        sslContextFactory.setKeyManagerPassword("changeit");
+        sslContextFactory.setKeyStorePath(KEYSTORE_PATH);
+        sslContextFactory.setKeyStoreType(KEYSTORE_TYPE);
+        sslContextFactory.setKeyStorePassword(KEYSTORE_PASSWORD);
+        sslContextFactory.setKeyManagerPassword(KEYSTORE_PASSWORD);
         sslContextFactory.setTrustAll(true);
 
         LOGGER.info("Creating SSL and HTTP connection factories");
@@ -47,7 +51,7 @@ public class ApiServer {
         LOGGER.info("Creating servlet context handler");
         ServletContextHandler servletContextHandler = new ServletContextHandler();
         servletContextHandler.setContextPath("/");
-        Path webRootPath = new File("tutorialapi-server/src/main/resources/web").toPath().toRealPath();
+        Path webRootPath = new File(WEB_ROOT_PATH).toPath().toRealPath();
         Resource baseResource = servletContextHandler.newResource(webRootPath.toUri());
         servletContextHandler.setBaseResource(baseResource);
         servletContextHandler.addServlet(DefaultServlet.class, "/");
