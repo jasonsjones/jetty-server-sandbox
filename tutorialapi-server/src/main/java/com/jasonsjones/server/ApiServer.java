ac770b6de3885e463b9dbad88600f492c827dea9
Jason Jones <jsjones96@gmail.com>
2024-09-14 19:45:25 -0700

    refactor: separate responsibilities in ApiServer

--- tutorialapi-server/src/main/java/com/jasonsjones/server/ApiServer.java
+++ tutorialapi-server/src/main/java/com/jasonsjones/server/ApiServer.java
@@ -1,9 +1,5 @@
 package com.jasonsjones.server;
 
-import java.io.File;
-import java.nio.file.Path;
-import java.util.Optional;
-
 import com.jasonsjones.rest.RestApi;
 import com.jasonsjones.server.config.ConfigKey;
 import com.jasonsjones.server.config.SystemKey;
@@ -19,6 +15,10 @@ import org.glassfish.jersey.servlet.ServletContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Optional;
+
 import static org.eclipse.jetty.http.HttpScheme.HTTPS;
 import static org.eclipse.jetty.http.HttpVersion.HTTP_1_1;
 
@@ -30,36 +30,47 @@ public class ApiServer {
     private static final String API_PATTERN = "/api/*";
     private static final String APPLICATION_INIT_PARAM_KEY = "jakarta.ws.rs.Application";
 
-    public static void main(String... args) throws Exception {
-        int port = Integer.parseInt(
-                Optional
-                        .ofNullable(System.getProperty(SystemKey.PORT.getKey()))
-                        .orElse(SystemKey.PORT.getDefaultValue()));
-        String mode = Optional
-                .ofNullable(System.getProperty(SystemKey.MODE.getKey()))
-                .orElse(SystemKey.MODE.getDefaultValue());
-
-        Config config = ConfigFactory.parseFile(new File(String.format(PROPERTY_FILE, mode)));
-
-        LOGGER.info("Setting up https configuration");
+    private static HttpConfiguration createHttpsConfiguration(int port) {
         HttpConfiguration httpsConfiguration = new HttpConfiguration();
         httpsConfiguration.setSecureScheme(HTTPS.asString());
         httpsConfiguration.setSecurePort(port);
         httpsConfiguration.addCustomizer(new SecureRequestCustomizer());
         httpsConfiguration.setSendServerVersion(false);
         httpsConfiguration.setSendDateHeader(false);
+        return httpsConfiguration;
+    }
 
-        LOGGER.info("Setting up SSL (server) context factory");
+    private static SslContextFactory.Server createSslContextFactory(Config config) {
         SslContextFactory.Server sslContextFactory = new SslContextFactory.Server();
         sslContextFactory.setKeyStorePath(config.getString(ConfigKey.SERVER_KEYSTORE_FILE.getKey()));
         sslContextFactory.setKeyStoreType(config.getString(ConfigKey.SERVER_KEYSTORE_TYPE.getKey()));
         sslContextFactory.setKeyStorePassword(config.getString(ConfigKey.SERVER_KEYSTORE_PASSWORD.getKey()));
         sslContextFactory.setKeyManagerPassword(config.getString(ConfigKey.SERVER_KEYSTORE_PASSWORD.getKey()));
         sslContextFactory.setTrustAll(true);
+        return sslContextFactory;
+    }
+
+    private static ServletContextHandler createServletContextHandler(Config config) throws Exception {
+        ServletContextHandler servletContextHandler = new ServletContextHandler();
+        servletContextHandler.setContextPath(ROOT_CONTEXT);
+        Path webRootPath = new File(config.getString(ConfigKey.SERVER_WEB_ROOT.getKey())).toPath().toRealPath();
+        Resource baseResource = servletContextHandler.newResource(webRootPath.toUri());
+        servletContextHandler.setBaseResource(baseResource);
+        servletContextHandler.addServlet(DefaultServlet.class, ROOT_CONTEXT);
+        return servletContextHandler;
+    }
+
+    private static Server createJettyServer(int port, Config config) throws Exception {
+        LOGGER.info("Setting up https configuration");
+        HttpConfiguration httpsConfiguration = createHttpsConfiguration(port);
+
+        LOGGER.info("Setting up SSL (server) context factory");
+        SslContextFactory.Server sslContextFactory = createSslContextFactory(config);
 
         LOGGER.info("Creating SSL and HTTP connection factories");
         SslConnectionFactory sslConnectionFactory = new SslConnectionFactory(sslContextFactory, HTTP_1_1.asString());
         HttpConnectionFactory httpsConnectionFactory = new HttpConnectionFactory(httpsConfiguration);
+
         Server server = new Server();
 
         LOGGER.info("Creating HTTPS connector");
@@ -67,19 +78,29 @@ public class ApiServer {
         httpsConnector.setPort(httpsConfiguration.getSecurePort());
 
         LOGGER.info("Creating servlet context handler");
-        ServletContextHandler servletContextHandler = new ServletContextHandler();
-        servletContextHandler.setContextPath(ROOT_CONTEXT);
-        LOGGER.info("Web root: {}", config.getString(ConfigKey.SERVER_WEB_ROOT.getKey()));
-        Path webRootPath = new File(config.getString(ConfigKey.SERVER_WEB_ROOT.getKey())).toPath().toRealPath();
-        Resource baseResource = servletContextHandler.newResource(webRootPath.toUri());
-        servletContextHandler.setBaseResource(baseResource);
-        servletContextHandler.addServlet(DefaultServlet.class, ROOT_CONTEXT);
+        ServletContextHandler servletContextHandler = createServletContextHandler(config);
 
         ServletHolder apiServletHolder = servletContextHandler.addServlet(ServletContainer.class, API_PATTERN);
         apiServletHolder.setInitParameter(APPLICATION_INIT_PARAM_KEY, RestApi.class.getName());
 
         server.addConnector(httpsConnector);
         server.setHandler(servletContextHandler);
+
+        return server;
+    }
+
+    public static void main(String... args) throws Exception {
+        int port = Integer.parseInt(
+                Optional
+                        .ofNullable(System.getProperty(SystemKey.PORT.getKey()))
+                        .orElse(SystemKey.PORT.getDefaultValue()));
+
+        String mode = Optional
+                .ofNullable(System.getProperty(SystemKey.MODE.getKey()))
+                .orElse(SystemKey.MODE.getDefaultValue());
+
+        Config config = ConfigFactory.parseFile(new File(String.format(PROPERTY_FILE, mode)));
+        Server server = createJettyServer(port, config);
 
         LOGGER.info("Starting app server on port {}", port);
         server.start();
