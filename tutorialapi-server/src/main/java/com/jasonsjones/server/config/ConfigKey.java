851ce3e4a55f966ebe58561b4e5c99d36bd4c318
Jason Jones <jsjones96@gmail.com>
2024-09-14 20:15:28 -0700

    refactor: add Key interface

--- tutorialapi-server/src/main/java/com/jasonsjones/server/config/ConfigKey.java
+++ tutorialapi-server/src/main/java/com/jasonsjones/server/config/ConfigKey.java
@@ -1,14 +1,8 @@
 package com.jasonsjones.server.config;
 
-import static java.util.Locale.ENGLISH;
-
-public enum ConfigKey {
+public enum ConfigKey implements Key {
     SERVER_KEYSTORE_FILE,
     SERVER_KEYSTORE_TYPE,
     SERVER_KEYSTORE_PASSWORD,
     SERVER_WEB_ROOT;
-
-    public String getKey() {
-        return name().toLowerCase(ENGLISH).replaceAll("_", ".");
-    }
 }
